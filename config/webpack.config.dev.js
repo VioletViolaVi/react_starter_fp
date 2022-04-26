b60f45fdb6a2ed689a485cb20c90a4e9b1de7304
Peritract <dan@getfutureproof.co.uk>
2022-04-26 12:09:59 +0100

    initial setup

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const config = require('./webpack.config.js');
+
+config.devServer = {
+  historyApiFallback: true,
+  contentBase: path.resolve('src'),
+  port: 8080,
+  hot: true,
+  open: false,
+  watchContentBase: true
+};
+
+config.devtool = 'inline-source-map';
+
+module.exports = config;
---
+++ config/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+    entry: path.resolve(__dirname, "../src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: 'bundle.js',
+        publicPath: "/",
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '../public/index.html'),
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif|pdf)$/,
+                use: ['file-loader'],
+            }
+        ]
+    },
+    mode: "none"
+};
+
+module.exports = config;
